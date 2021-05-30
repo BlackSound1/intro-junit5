1198496cf8a40470236da026d39c426d9a1c6c01
BlackSound1 <matthewr@videotron.ca>
2021-05-30 13:13:00 -0400

    set up BeforeAll and AfterAll tests

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,15 +1,22 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
     private Greeting greeting;
 
+    @BeforeAll
+    static void beforeAll() {
+        // Runs before anything in the Test class
+        // Needs to be static
+        System.out.println("Before all. Only called once!");
+    }
+
     @BeforeEach
     void setUp() {
+        System.out.println("In before each...");
         // Sets up a new Greeting object BeforeEach Test
         // So we don't need to write it each time
         greeting = new Greeting();
@@ -23,5 +30,17 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("dude"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        // Runs after everything in the Test class
+        // Needs to be static
+        System.out.println("After all. Only called once!");
     }
 }
